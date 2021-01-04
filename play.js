edc86d445a7948424ce71fe5ebb0eea22b3d2552
P-oke <63716839+P-oke@users.noreply.github.com>
2021-01-04 11:53:51 +0100

    Delete play.js

--- play.js
+++
@@ -1,6 +0,0 @@
-const tog = document.getElementsByClassName("togglebar")[0];
-const links = document.getElementsByTagName("nav")[0];
-tog.addEventListener("click", () => {
-  links.classList.toggle("active");
-});
-//hello
